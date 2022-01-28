5c5b4fa5f62082a9db4bfb404b8b2d00eb07d21d
Zoran Pesic <zoran1991@gmail.com>
2022-01-28 00:12:25 -0800

    make gem configurable, add api_key config

--- lib/usda_fdc.rb
+++ lib/usda_fdc.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require_relative 'usda_fdc/version'
+require_relative 'usda_fdc/configuration'
 
 module UsdaFdc
+  extend Configuration
+
+  define_setting :api_key
 end
