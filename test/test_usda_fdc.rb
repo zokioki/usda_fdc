988073bd898db8e9bb58dfcf557afa9df8c1a909
Zoran Pesic <zoran1991@gmail.com>
2022-01-28 01:12:37 -0800

    remove sample failing test

--- test/test_usda_fdc.rb
+++ test/test_usda_fdc.rb
@@ -6,8 +6,4 @@ class TestUsdaFdc < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::UsdaFdc::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
