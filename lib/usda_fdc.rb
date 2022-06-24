d47b0dfacdce8a10d8d3ed2b4893d004c10db348
Zoran Pesic <zoran1991@gmail.com>
2022-06-24 00:18:11 -0700

    add support for interacting with USDA FDC endpoints

--- lib/usda_fdc.rb
+++ lib/usda_fdc.rb
@@ -2,6 +2,7 @@
 
 require_relative 'usda_fdc/version'
 require_relative 'usda_fdc/configuration'
+require_relative 'usda_fdc/client'
 
 module UsdaFdc
   extend Configuration
---
+++ lib/usda_fdc/client.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
+module UsdaFdc
+  class Client
+    API_URL = 'https://api.nal.usda.gov'
+
+    def initialize(api_key)
+      @api_key = api_key || UsdaFdc.api_key
+    end
+
+    def food(fdc_id, params = {})
+      get("/fdc/v1/food/#{fdc_id}", params)
+    end
+
+    def foods(body)
+      post('/fdc/v1/foods', body)
+    end
+
+    def foods_list(body)
+      post('/fdc/v1/foods/list', body)
+    end
+
+    def foods_search(body)
+      post('/fdc/v1/foods/search', body)
+    end
+
+    def get(path, params = {})
+      uri = URI("#{API_URL}#{path}")
+      uri.query = URI.encode_www_form(params) if params.any?
+
+      request = Net::HTTP::Get.new(uri)
+      request['X-Api-Key'] = @api_key
+      request['Content-Type'] = 'application/json'
+
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(request)
+      end
+
+      JSON.parse(response.body)
+    end
+
+    def post(path, body = {})
+      uri = URI("#{API_URL}#{path}")
+
+      request = Net::HTTP::Post.new(uri)
+      request['X-Api-Key'] = @api_key
+      request['Content-Type'] = 'application/json'
+      request.body = body.to_json
+
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(request)
+      end
+
+      JSON.parse(response.body)
+    end
+  end
+end
